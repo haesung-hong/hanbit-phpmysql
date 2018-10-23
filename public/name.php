65ce0d7d06a2f57b678088357fda9d44db949ba8
cloudshadow <cloudshadow@gmail.com>
2018-10-23 20:46:57 +0900

    20181023 중간저장

--- public/name.php
+++ public/name.php
@@ -1,6 +1,6 @@
 <?php
 
 $name = $_GET['name'];
-echo 'Welcome to our website, ' .  htmlspecialchars($name, ENT_QUOTES, 'UTF-8') . '!';
+echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8') . '님, 홈페이지 방문을 환영합니다!';
 
 ?>
