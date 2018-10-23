5c1e6d2bda5db1a30f931c96efafa439da9a7712
cloudshadow <cloudshadow@gmail.com>
2018-10-23 20:47:07 +0900

    20181023 중간저장

--- classes/Ijdb/Controllers/Joke.php
+++ classes/Ijdb/Controllers/Joke.php
@@ -1,7 +1,7 @@
 <?php
 namespace Ijdb\Controllers;
-use \Ninja\DatabaseTable;
-use \Ninja\Authentication;
+use \Hanbit\DatabaseTable;
+use \Hanbit\Authentication;
 
 class Joke {
 	private $authorsTable;
@@ -32,7 +32,7 @@ class Joke {
 		}
 
 
-		$title = 'Joke list';
+		$title = '유머글 목록';
 
 		$totalJokes = $this->jokesTable->total();
 
@@ -49,7 +49,7 @@ class Joke {
 	}
 
 	public function home() {
-		$title = 'Internet Joke Database';
+		$title = '인터넷 유머 세상';
 
 		return ['template' => 'home.html.php', 'title' => $title];
 	}
