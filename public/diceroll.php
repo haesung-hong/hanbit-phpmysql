4f82cdebe36a03096d62e1677806da03b3365e7c
cloudshadow <cloudshadow@gmail.com>
2018-10-23 20:46:56 +0900

    20181023 중간저장

--- public/diceroll.php
+++ public/diceroll.php
@@ -3,14 +3,14 @@
 $roll = 0;
 while ($roll != 6) {
   $roll = rand(1, 6);
-  echo '<p>You rolled a ' . $roll . '</p>';
+  echo '<p>주사위를 굴려서 나온 숫자 : ' . $roll . '</p>';
 
 
   if ($roll == 6) {
-    echo '<p>You win!</p>';
+    echo '<p>이겼다!</p>';
   }
   else {
-    echo '<p>Sorry, you didn\'t win, better luck next time!</p>';
+    echo '<p>아쉽지만 \'꽝\'이네요. 다음 기회를 노려보세요!</p>';
   }
 }
 ?>
