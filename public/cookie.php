ea808a7815e38eed8851431994577b2757918266
cloudshadow <cloudshadow@gmail.com>
2019-01-16 10:08:47 +0900

    20190114 한글화

--- public/cookie.php
+++ public/cookie.php
@@ -10,10 +10,10 @@ setcookie('visits', $visits, time() + 3600 * 24 * 365);
 
 if ($visits > 1)
 {
-  echo "This is visit number $visits.";
+  echo "$visits 번째 방문하셨습니다.";
 }
 else
 {
-  // First visit
-  echo 'Welcome to my website! Click here for a tour!';
+  // 첫 방문
+  echo '웹사이트에 오신 걸 환영합니다! 둘러보려면 여기를 클릭하세요!';
 }
