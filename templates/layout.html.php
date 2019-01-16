3c3e0985692c983506a1a72ae848eabe723093bf
cloudshadow <cloudshadow@gmail.com>
2019-01-16 10:06:09 +0900

    20190114 한글화

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -12,8 +12,8 @@
     </header>
     <ul>
       <li><a href="/">Home</a></li>
-      <li><a href="/joke/list">Jokes List</a></li>
-      <li><a href="/joke/edit">Add a new Joke</a></li>
+      <li><a href="/joke/list">유머 글 목록</a></li>
+      <li><a href="/joke/edit">유머 글 등록</a></li>
     </ul>
   </nav>
 
