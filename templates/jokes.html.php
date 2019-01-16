ae7065ddca56b42dfec51a57687d1ebae4bb5415
cloudshadow <cloudshadow@gmail.com>
2019-01-16 10:06:13 +0900

    20190114 한글화

--- templates/jokes.html.php
+++ templates/jokes.html.php
@@ -3,7 +3,7 @@
   <p>
   <?=htmlspecialchars($joke['joketext'], ENT_QUOTES, 'UTF-8')?>
 
-  (by <a href="mailto:<?php
+  (작성자: <a href="mailto:<?php
                 echo htmlspecialchars($joke['email'], ENT_QUOTES,
                     'UTF-8'); ?>"><?php
                 echo htmlspecialchars($joke['name'], ENT_QUOTES,
@@ -11,7 +11,7 @@
   
   <form action="deletejoke.php" method="post">
     <input type="hidden" name="id" value="<?=$joke['id']?>">
-    <input type="submit" value="Delete">
+    <input type="submit" value="삭제">
   </form>
   </p>
 </blockquote>
