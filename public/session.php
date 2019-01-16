fbc16b3d4e8e8fee44b8998018138964a6282e7c
cloudshadow <cloudshadow@gmail.com>
2019-01-16 10:08:50 +0900

    20190114 한글화

--- public/session.php
+++ public/session.php
@@ -11,10 +11,10 @@ $_SESSION['visits'] = $_SESSION['visits'] + 1;
 
 if ($_SESSION['visits'] > 1)
 {
-  echo 'This is visit number ' . $_SESSION['visits'];
+  echo $_SESSION['visits'] . " 번째 방문하셨습니다.";
 }
 else
 {
-  // First visit
-  echo 'Welcome to my website! Click here for a tour!';
+  // 첫 방문
+  echo '웹사이트에 오신 걸 환영합니다! 둘러보려면 여기를 클릭하세요!';
 }
