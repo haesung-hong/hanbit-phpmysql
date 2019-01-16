15f9eb6b39ee3edc677944f0f38dcaeba997761a
cloudshadow <cloudshadow@gmail.com>
2019-01-16 10:06:06 +0900

    20190114 한글화

--- classes/Hanbit/DatabaseTable.php
+++ classes/Hanbit/DatabaseTable.php
@@ -74,7 +74,7 @@ class DatabaseTable {
 
 		$query .= ' WHERE `' . $this->primaryKey . '` = :primaryKey';
 
-		//Set the :primaryKey variable
+		// :primaryKey 변수 설정
 		$fields['primaryKey'] = $fields['id'];
 
 		$fields = $this->processDates($fields);
@@ -99,7 +99,7 @@ class DatabaseTable {
 	private function processDates($fields) {
 		foreach ($fields as $key => $value) {
 			if ($value instanceof DateTime) {
-				$fields[$key] = $value->format('Y-m-d');
+				$fields[$key] = $value->format('Y-m-d H:i:s');
 			}
 		}
 
