d85804b49e6a9e6654714592bcbb8c5a641337f4
cloudshadow <cloudshadow@gmail.com>
2019-01-16 10:09:05 +0900

    20190114 한글화

--- templates/jokes.html.php
+++ templates/jokes.html.php
@@ -1,12 +1,12 @@
 
-<p><?=$totalJokes?> jokes have been submitted to the Internet Joke Database.</p>
+<p><?=$totalJokes?>개 유머 글이 있습니다.</p>
 
 <?php foreach($jokes as $joke): ?>
 <blockquote>
   <p>
   <?=htmlspecialchars($joke['joketext'], ENT_QUOTES, 'UTF-8')?>
 
-  (by <a href="mailto:<?php
+  (작성자: <a href="mailto:<?php
                 echo htmlspecialchars($joke['email'], ENT_QUOTES,
                     'UTF-8'); ?>"><?php
                 echo htmlspecialchars($joke['name'], ENT_QUOTES,
@@ -14,7 +14,7 @@
   
   <form action="deletejoke.php" method="post">
     <input type="hidden" name="id" value="<?=$joke['id']?>">
-    <input type="submit" value="Delete">
+    <input type="submit" value="삭제">
   </form>
   </p>
 </blockquote>
